0b894154cc9a59cd40d2d72aa481c90eadd08f1e
George Webb <gwebbgit@users.noreply.github.com>
2019-12-19 15:51:14 -1000

    mlx lighting glitch
    working on it
    coming from tpmc3x

---
+++ SIMPL/SPlsWork/Logger3.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Linq;
+using Crestron;
+using Crestron.Logos.SplusLibrary;
+using Crestron.Logos.SplusObjects;
+using Crestron.SimplSharp;
+
+namespace UserModule_LOGGER3
+{
+    public class UserModuleClass_LOGGER3 : SplusObject
+    {
+        static CCriticalSection g_criticalSection = new CCriticalSection();
+        InOutArray<Crestron.Logos.SplusObjects.DigitalInput> IN;
+        object IN_OnChange_0 ( Object __EventInfo__ )
+        
+            { 
+            Crestron.Logos.SplusObjects.SignalEventArgs __SignalEventArg__ = (Crestron.Logos.SplusObjects.SignalEventArgs)__EventInfo__;
+            try
+            {
+                SplusExecutionContext __context__ = SplusThreadStartCode(__SignalEventArg__);
+                ushort I = 0;
+                
+                
+                __context__.SourceCodeLine = 4;
+                I = (ushort) ( Functions.GetLastModifiedArrayIndex( __SignalEventArg__ ) ) ; 
+                __context__.SourceCodeLine = 5;
+                GenerateUserNotice ( "{0}: #{1:d}={2:d}\r\n", GetSymbolInstanceName ( ) , (ushort)I, (ushort)IN[ I ] .Value) ; 
+                
+                
+            }
+            catch(Exception e) { ObjectCatchHandler(e); }
+            finally { ObjectFinallyHandler( __SignalEventArg__ ); }
+            return this;
+            
+        }
+        
+    
+    public override void LogosSplusInitialize()
+    {
+        SocketInfo __socketinfo__ = new SocketInfo( 1, this );
+        InitialParametersClass.ResolveHostName = __socketinfo__.ResolveHostName;
+        _SplusNVRAM = new SplusNVRAM( this );
+        
+        IN = new InOutArray<DigitalInput>( 3, this );
+        for( uint i = 0; i < 3; i++ )
+        {
+            IN[i+1] = new Crestron.Logos.SplusObjects.DigitalInput( IN__DigitalInput__ + i, IN__DigitalInput__, this );
+            m_DigitalInputList.Add( IN__DigitalInput__ + i, IN[i+1] );
+        }
+        
+        
+        for( uint i = 0; i < 3; i++ )
+            IN[i+1].OnDigitalChange.Add( new InputChangeHandlerWrapper( IN_OnChange_0, false ) );
+            
+        
+        _SplusNVRAM.PopulateCustomAttributeList( true );
+        
+        NVRAM = _SplusNVRAM;
+        
+    }
+    
+    public override void LogosSimplSharpInitialize()
+    {
+        
+        
+    }
+    
+    public UserModuleClass_LOGGER3 ( string InstanceName, string ReferenceID, Crestron.Logos.SplusObjects.CrestronStringEncoding nEncodingType ) : base( InstanceName, ReferenceID, nEncodingType ) {}
+    
+    
+    
+    
+    const uint IN__DigitalInput__ = 0;
+    
+    [SplusStructAttribute(-1, true, false)]
+    public class SplusNVRAM : SplusStructureBase
+    {
+    
+        public SplusNVRAM( SplusObject __caller__ ) : base( __caller__ ) {}
+        
+        
+    }
+    
+    SplusNVRAM _SplusNVRAM = null;
+    
+    public class __CEvent__ : CEvent
+    {
+        public __CEvent__() {}
+        public void Close() { base.Close(); }
+        public int Reset() { return base.Reset() ? 1 : 0; }
+        public int Set() { return base.Set() ? 1 : 0; }
+        public int Wait( int timeOutInMs ) { return base.Wait( timeOutInMs ) ? 1 : 0; }
+    }
+    public class __CMutex__ : CMutex
+    {
+        public __CMutex__() {}
+        public void Close() { base.Close(); }
+        public void ReleaseMutex() { base.ReleaseMutex(); }
+        public int WaitForMutex() { return base.WaitForMutex() ? 1 : 0; }
+    }
+     public int IsNull( object obj ){ return (obj == null) ? 1 : 0; }
+}
+
+
+}
